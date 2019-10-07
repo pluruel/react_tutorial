7037f88cfc03fbc66d16c9601c9f342850195cd3
juno <juno@localhost.localdomain>
2019-10-07 04:02:35 -0500

    update

---
+++ redux-middleware/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import CounterContainer from './containers/CounterContainer';
+import SampleContainer from './containers/SampleContainer';
+
+function App() {
+  return (
+    <>
+      <SampleContainer />
+    </>
+  );
+}
+
+export default App;
---
+++ redux-middleware/src/modules/counter.js
@@ -0,0 +1,30 @@
+import { createAction, handleActions } from 'redux-actions';
+const INCREASE = 'counter/INCREASE';
+const DECREASE = 'counter/DECREASE';
+
+export const increase = createAction(INCREASE);
+export const decrease = createAction(DECREASE);
+
+const init = 0;
+
+export const increaseAsync = () => dispatch => {
+  setTimeout(() => {
+    dispatch(increase());
+  }, 1000);
+};
+
+export const decreaseAsync = () => dispatch => {
+  setTimeout(() => {
+    dispatch(decrease());
+  }, 1000);
+};
+
+const counter = handleActions(
+  {
+    [INCREASE]: state => state + 1,
+    [DECREASE]: state => state - 1,
+  },
+  init,
+);
+
+export default counter;
